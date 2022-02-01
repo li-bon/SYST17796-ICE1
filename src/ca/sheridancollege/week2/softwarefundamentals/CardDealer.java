b749b7190961d44e6242839f24c7baef491040f3
li-bon <libon@sheridancollege.ca>
2022-02-01 13:23:28 -0500

    added new object in CardDealer.java

--- src/ca/sheridancollege/week2/softwarefundamentals/CardDealer.java
+++ src/ca/sheridancollege/week2/softwarefundamentals/CardDealer.java
@@ -4,15 +4,18 @@
  * and open the template in the editor.
  */
 package ca.sheridancollege.week2.softwarefundamentals;
-//cloned by Bonita on Feb 1 2022
+/*cloned by Bonita on Feb 1 2022
+*Student number: 991358694
+*/
 /**
  * The class where we create Cards
  * @author dancye, 2019
  */
 public class CardDealer 
 {
-    public static void main(String[] args)
+    public static void main(String[] args)  
     {
+        Card bonita = new Card("Spade",4);
         Card nineClubs = new Card("Clbs", 9);
         nineClubs.setSuit("diamonds");
     }
